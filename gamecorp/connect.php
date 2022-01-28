d80941043c7f734747f5d518892e532123378ff5
Muhammad Danial <68473358+dandeviant@users.noreply.github.com>
2022-01-29 01:36:46 +0800

    Delete gamecorp directory

--- gamecorp/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$connection = mysqli_connect('localhost', 'root', ''); //The Blank string is the password
-	mysqli_select_db($connection, 'staffs');
-
-	if ($connection->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-?>
